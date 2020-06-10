6ba519a9dda5cb39af61bf8ee90d497ef07e6d63
RegChien <baisheng10081@icloud.com>
2020-06-10 10:16:52 +0800

    Initial Commit

---
+++ MX-Editor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MX-Editor
+//
+//  Created by RegChien on 2020/6/10.
+//  Copyright © 2020 RegChien. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
